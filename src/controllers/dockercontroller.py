10a6a5d02cf1d9e9eee2c177249bf32a1cd45537
fls <fls>
2016-01-24 11:55:14 +0800

    add docker

---
+++ src/controllers/dockercontroller.py
@@ -0,0 +1,137 @@
+# -*- coding: UTF-8 -*- 
+import json
+import os
+from flask import Module,render_template,jsonify,request,g
+from src.dcosconfig import *
+docker = Module(__name__)
+@docker.route('/Container/ServerModel',methods=['POST'])
+#config docker profile
+def createContainerServerModel ():
+        nickname = request.json['NickName']
+        imagetype = request.json['ImageType']
+        imagename = request.json['ImageName']
+        containerport = request.json['ContainerPort']    
+        hostport = request.json['HostPort']
+        containerpath = request.json['ContainerPath']
+        hostpath = request.json['HostPath']
+#         pathList = request.json['PathList']
+#     try:
+#         containerpath = request.json['ContainerPath']
+#         hostpath = request.json['HostPath']
+#     except KeyError,e:
+#         containerpath = ''
+#         hostpath = ''
+#     if (request.json['ProjectName'] != '') & (request.json['ProjectKey'] != ''):
+#         exist = dockerservice.create(request.json['ProjectName'],request.json['ProjectKey'],Introduction,g.user_id)
+#         empty = False
+#     else :
+#         exist = True
+#         empty = True
+#     exist_json = {'exist': exist, 'empty': empty}
+#         return jsonify("Ture")
+        return 'ok'  
+    
+    
+@docker.route('/ServerModel/Update',methods=['POST'])
+def serverModelUpdate():
+    ProjectId = request.json['ProjectId']
+    IssueId = request.json['IssueId']
+    subject = request.json['Subject']
+    projectmodule_id = request.json['ProjectModuleId']
+    assign_to = request.json['AssignTo']
+    if int(assign_to) == -1:
+        assign_to = g.user_id
+    priority = request.json['Priority']
+    category_id = request.json['CategoryId']
+    status = request.json['Status']
+    feedback = request.json['Feedback']
+    issueservice.update(ProjectId,IssueId,subject,projectmodule_id,category_id,assign_to,priority,status,feedback,g.user_id)
+    return jsonify(updated=True)
+
+@docker.route('/ServerModel/Delete',methods=['POST'])
+def serverModelDelete():
+    issueservice.delete(request.json['IssueId'])
+    return jsonify(deleted=True)    
+def serverModellist(servermodel_id):
+    member_list = teamservice.member_in_project(servermodel_id)
+    category = issueservice.available_category()
+    return render_template('ServerModel/List.html',ProjectId=project_id,MemberList=member_list,Category=category)    
+
+
+@docker.route('/Container/ResourceModel',methods=['POST'])
+def createContainerResourceModel ():
+        nickname = request.json['NickName']
+        case = request.json['Case']
+        dockercpu = request.json['DockerCpu']
+        dockermemory = request.json['DockerMemory']
+        dockervolume = request.json['DockerVolume']  
+        return 'ok' 
+    
+
+@docker.route('/ServerModel/Update',methods=['POST'])
+def resourceModelUpdate():
+    ProjectId = request.json['ProjectId']
+    IssueId = request.json['IssueId']
+    subject = request.json['Subject']
+    projectmodule_id = request.json['ProjectModuleId']
+    assign_to = request.json['AssignTo']
+    if int(assign_to) == -1:
+        assign_to = g.user_id
+    priority = request.json['Priority']
+    category_id = request.json['CategoryId']
+    status = request.json['Status']
+    feedback = request.json['Feedback']
+    issueservice.update(ProjectId,IssueId,subject,projectmodule_id,category_id,assign_to,priority,status,feedback,g.user_id)
+    return jsonify(updated=True)
+
+@docker.route('/ServerModel/Delete',methods=['POST'])
+def resourceDelete():
+    issueservice.delete(request.json['IssueId'])
+    return jsonify(deleted=True)    
+def resourceModellist(servermodel_id):
+    member_list = teamservice.member_in_project(servermodel_id)
+    category = issueservice.available_category()
+    return render_template('ServerModel/List.html',ProjectId=project_id,MemberList=member_list,Category=category)    
+
+
+@docker.route('/Container/MergeModel',methods=['POST'])     
+def MergeContainerProfile(servermodelid,resourcemodelid):
+        try:
+           readed = json.load(open('src/static/model/docker/temp.json', 'r'))
+        except Exception,e:
+           print Exception,':',e
+        readed['container']['docker']['image'] = createContainerServerModel.imagename
+        name = readed['id'] 
+        json.dump(readed, open('src/static/model/docker/app.json', 'w'))
+        return 'ok'    
+@docker.route('/Container/commit',methods=['POST'])     
+def createContainerProfile():
+        
+        try:
+           readed = json.load(open('src/static/model/docker/temp.json', 'r'))
+        except Exception,e:
+           print Exception,':',e
+        readed['container']['docker']['image'] = createContainerServerModel.imagename
+        name = readed['id'] 
+        json.dump(readed, open('src/static/model/docker/app.json', 'w'))
+        return 'ok'
+def query():
+    subject = request.json['Subject']
+    assign_to = int(request.json['AssignTo'])
+    if assign_to == -1:
+        assign_to = g.user_id
+    category_id = int(request.json['CategoryId'])
+    status_open = request.json['Open']
+    status_fixed = request.json['Fixed']
+    status_closed = request.json['Closed']
+    status_canceled = request.json['Canceled']
+    page_no = request.json['PageNo']
+    (row_count,page_count,page_no,page_size,data) = issueservice.query(subject,assign_to,category_id,status_open,status_fixed,status_closed,status_canceled,'CreateDate',page_no,PAGESIZE_issue,g.user_id)
+    issue_list = []
+    for i in data.all():
+        issue_list.append({'IssueId':i.IssueId,'ProjectId':i.ProjectId,'ProjectKey':i.ProjectProfile.ProjectKey,'Category':i.Category.CategoryName,'Subject':i.Subject,'Priority':i.Priority,'Status':i.Status,'AssignTo':i.AssignToProfile.Nick,'Creator':i.CreatorProfile.Nick,'LastUpdateDate':i.LastUpdateDate.isoformat()})
+    return jsonify(row_count=row_count,page_count=page_count,page_no=page_no,page_size=page_size,data=issue_list)
+    
+if __name__ == '__main__':
+    createContainerProfile()    
+    
---
+++ src/models/docker.py
@@ -0,0 +1,34 @@
+from src.models.database import BaseModel
+from sqlalchemy import Column,DateTime,NVARCHAR,Integer,ForeignKey,UnicodeText
+class docker(BaseModel):
+
+    __tablename__ = 'docker'
+    DockerId = Column('DockerId', Integer,primary_key=True,nullable=False,autoincrement=True)
+    TaskName = Column('TaskName', NVARCHAR(30),nullable = False)
+    SCMUrl = Column('SCMUrl', NVARCHAR(30),nullable = False)
+    Poll = Column('Poll', NVARCHAR(30),nullable = False)
+    Shell = Column('Shell', NVARCHAR(30),nullable = False)
+    TypeStatus = Column('TypeStatus', Integer,nullable=False)
+    CREATEDATE = Column('CREATEDATE', DateTime,nullable=False)
+class dockerServerModel(BaseModel):
+
+    __tablename__ = 'dockerServerModel'
+    ServiceModeld = Column('ServiceModeld', Integer,primary_key=True,nullable=False,autoincrement=True)
+    NickName = Column('NickName', NVARCHAR(30),nullable = False)
+    ImageType = Column('ImageType', NVARCHAR(30),nullable = False)
+    ImageName = Column('ImageName', NVARCHAR(30),nullable = False)
+    ContainerPort = Column('ContainerPort', Integer,nullable = False)
+    HostPort = Column('HostPort', Integer,nullable=False)
+    ContainerPath = Column('ContainerPath', NVARCHAR(30),nullable = False)
+    HostPath = Column('HostPath', NVARCHAR(30),nullable = False)    
+    CREATEDATE = Column('CREATEDATE', DateTime,nullable=False)
+class dockerResourceModel(BaseModel):
+
+    __tablename__ = 'dockerResourceModel'
+    ResourceModelId = Column('ResourceModelId', Integer,primary_key=True,nullable=False,autoincrement=True)
+    NickName = Column('NickName', NVARCHAR(30),nullable = False)
+    Case = Column('Case', Integer,nullable=False)
+    DockerCpu = Column('DockerCpu', NVARCHAR(30),nullable = False)
+    DockerMemory = Column('DockerMemory', Integer,nullable = False)
+    DockerVolume = Column('DockerVolume', Integer,nullable=False)
+    CREATEDATE = Column('CREATEDATE', DateTime,nullable=False)    
