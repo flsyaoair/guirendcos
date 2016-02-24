7090e259c7c7f7f11c361aeb3db3635745b57465
fls <fls>
2016-02-24 10:08:35 +0800

    test

--- src/collect/__init__.py
+++ src/collect/__init__.py
@@ -1,3 +1,2 @@
 # -*- coding: UTF-8 -*- 
-from src.collect.mesoscollet import mesos
-from src.controllers.marathoncollet import marathon
+from src.collect.jenkinscontroller import jenkins
--- src/collect/jenkinscontroller.py
+++ src/collect/jenkinscontroller.py
@@ -1,54 +1,33 @@
 import os
 import jenkinsapi
+from template import *
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column,DateTime,NVARCHAR,Integer,ForeignKey,UnicodeText,SMALLINT
 from marathon import MarathonClient
-import template
 from jenkinsapi.jenkins import Jenkins 
-# from jenkinsapi.build import Build 
-# from jenkinsapi import api
-from flask import Flask,request,url_for
-common = 0
-release = 1
-marathonip = 'http://20.26.17.133:8080'
-# DB ='mysql+mysqlconnector://root:root@20.26.17.145:3306/dcos?charset=utf8'
-DB ='mysql+mysqlconnector://root:root@127.0.0.1:3306/dcos?charset=utf8'
-
-app = Flask(__name__)
-engine = create_engine(DB,echo=True)
- 
-BaseModel = declarative_base()
-c = MarathonClient(marathonip)
-
-def create_database():
-    BaseModel.metadata.create_all(bind=engine)
-def drop_database():
-    BaseModel.metadata.drop_all(bind=engine)
-def get_session():
-    
-    return Session(bind = engine)
-@app.route('/')
-def hello_Jenkins():
-    return 'Hello Jenkins!'
+from flask import Module,render_template,Flask,request,url_for
+from src.dcosconfig import *
+jenkins = Module(__name__)
 
  
 def get_server_instance():
-    server = Jenkins('http://20.26.17.145:8089/jenkins')
+    server = Jenkins(JENKINS)
 #     server = Jenkins('http://127.0.0.1:8080')
     return server
-# @app.route('/jenkins/job/<taskcasename>')
+@jenkins.route('/jenkins')
 def get_job_details():
     # Refer Example #1 for definition of function 'get_server_instance'
-    server = get_server_instance()
-    for j in server.get_jobs():
-        job_instance = server.get_job(j[0])
-        print 'Job Name:%s' %(job_instance.name)
-        print 'Job Description:%s' %(job_instance.get_description())
-        print 'Is Job running:%s' %(job_instance.is_running())
-        print 'Is Job enabled:%s' %(job_instance.is_enabled())
-@app.route('/jenkins/job/stop/<taskcasename>')        
+#     server = get_server_instance()
+#     for j in server.get_jobs():
+#         job_instance = server.get_job(j[0])
+#         print 'Job Name:%s' %(job_instance.name)
+#         print 'Job Description:%s' %(job_instance.get_description())
+#         print 'Is Job running:%s' %(job_instance.is_running())
+#         print 'Is Job enabled:%s' %(job_instance.is_enabled())
+    return render_template('CI/Create.html')   
+@jenkins.route('/jenkins/job/stop/<taskcasename>',methods=['POST'])        
 def disable_job():
     # Refer Example #1 for definition of function 'get_server_instance'
     server = get_server_instance()
@@ -68,7 +47,7 @@ def get_plugin_details():
         print "URL:%s" %(plugin.url)
         print "Active:%s" %(plugin.active)
         print "Enabled:%s" %(plugin.enabled) 
-@app.route('/jenkins/job/build/<taskcasename>')            
+@jenkins.route('/jenkins/job/build/<taskcasename>')            
 def job_build():
     # Refer Example #1 for definition of function 'get_server_instance'
     server = get_server_instance()
@@ -77,14 +56,14 @@ def createjob():
     # Refer Example #1 for definition of function 'get_server_instance'
     server = get_server_instance()
 #     server.create_job('jobname', xml)
-@app.route('/jenkins/job/delete/<taskcasename>')
+@jenkins.route('/jenkins/job/delete/<taskcasename>')
 def deletejob():
     # Refer Example #1 for definition of function 'get_server_instance'
     server = get_server_instance()
     server.delete_job('jobname2')
 
     
-@app.route('/jenkins/job/')    
+@jenkins.route('/jenkins/job/')    
 def createjobconfig():
 #     print request.data
 #     print request.form()
@@ -93,13 +72,13 @@ def createjobconfig():
     shell = 'sleep 5'
     typestatus = 0
     server = get_server_instance()
-    config = template.jenkinsconfigfile
+    config = jenkinsconfigfile
     config = config.replace('%SCMPATH%',scmurl)
     config = config.replace('%POLL%',poll )
     config = config.replace('%SHELL%', shell)
     server.create_job('jobname2', config)
     return 'ok'
-@app.route('/jenkins/job/update/<taskcasename>')
+@jenkins.route('/jenkins/job/update/<taskcasename>')
 def updatejobconfig():
     scmurl = 'svn://20.26.19.69/home/svn/svn_repository/Program/script/release'
     poll = 'H/5 * * * *'
@@ -107,7 +86,7 @@ def updatejobconfig():
     typestatus = 0
     server = get_server_instance()
     job = server.get_job("jobname2")
-    config = template.jenkinsconfigfile
+    config = jenkinsconfigfile
     config = config.replace('%SCMPATH%',scmurl)
     config = config.replace('%POLL%',poll )
     config = config.split('\n')
@@ -119,54 +98,9 @@ def updatejobconfig():
     config = config.replace('%SHELL%', shell)  
     job.update_config(config)
     print 'ok'   
-class JenkinsTask(BaseModel):
 
-    __tablename__ = 'JenkinsTask'
-    JenkinsTaskId = Column('JenkinsTaskId', Integer,primary_key=True,nullable=False,autoincrement=True)
-    TaskName = Column('TaskName', NVARCHAR(30),nullable = False)
-    SCMUrl = Column('SCMUrl', NVARCHAR(30),nullable = False)
-    Poll = Column('Poll', NVARCHAR(30),nullable = False)
-    Shell = Column('Shell', NVARCHAR(30),nullable = False)
-    TypeStatus = Column('TypeStatus', Integer,nullable=False)
-
-def servicesCreate(taskname,scmurl,poll,shell,typestatus):
-#     database = get_session()
-    session = get_session()
-    T = JenkinsTask()
-    T.TaskName = taskname
-    T.SCMUrl = scmurl
-    T.Poll = poll
-    T.Shell = shell
-    T.TypeStatus = typestatus
-    session.add(T)
-    session.commit()
-    session.close()
-def delete(id):
-    session = get_session()
-    session.query(JenkinsTask).filter(JenkinsTask.JenkinsTaskId == id).delete()
-    session.commit()
-    session.close()    
-def update(id,taskname,scmurl,poll,shell):
-#     taskcasename ='cc'
-    session = get_session()
-
-    task = session.query(JenkinsTask).filter(JenkinsTask.JenkinsTaskId == id).update({'TaskName':taskname,'SCMUrl':scmurl,'Poll':poll,'Shell':shell})
-
-    session.commit()
-    session.close()
-with app.test_request_context():
-    print  url_for('createjobconfig', next="dd")    
+# with jenkins.test_request_context():
+#     print  url_for('createjobconfig', next="dd")    
 if __name__ == '__main__':
-#     print get_server_instance().version 
-#     get_job_details() 
-#     disable_job()
-#     get_plugin_details()
-#     job_build()
-#     createjob()  
-#     createjobconfig()
-    create_database()
 
-#     updatejobconfig()
-#     app.run(host= '10.73.144.234',port=5002)
-#     app.run(host= '10.70.86.39',port=5002)
-    app.run(host= '127.0.0.1',port=5002)
+   print 'ok'
