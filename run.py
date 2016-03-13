f23ecff4ca6eb353dedbb8408a63a37f65b76368
yly <yly>
2016-03-13 14:39:16 +0800

    修改dashboard

--- run.py
+++ run.py
@@ -5,6 +5,6 @@ from src.dcosconfig import *
 app = create_dcos_app()
 
 if __name__ == '__main__':
-    app.debug = False
+    app.debug = True
     app.run(host= HOST,port=PORT)
 
--- src/controllers/dashboard.py
+++ src/controllers/dashboard.py
@@ -7,4 +7,48 @@ dashboard.before_request(login_filter)
 
 @dashboard.route('/Dashboard')
 def index():
-    return render_template('Dashboard/index.html')
+    menuList = {
+        'index': {
+            'main': 'active'
+        },
+
+        'template': {
+            'main': '',
+            'sub1': '',
+            'sub2': '',
+        }
+    }
+    return render_template('Dashboard/index.html',menu = menuList)
+
+@dashboard.route('/template/container')
+def container():
+    menuList = {
+        'index': {
+            'main': ''
+        },
+
+        'template': {
+            'main': 'active',
+            'sub1': 'active',
+            'sub2': '',
+        }
+    }
+
+    return render_template('template/container.html', menu = menuList)
+
+
+@dashboard.route('/template/service')
+def service():
+    menuList = {
+        'index': {
+            'main': ''
+        },
+
+        'template': {
+            'main': 'active',
+            'sub1': '',
+            'sub2': 'active',
+        }
+    }
+
+    return render_template('template/service.html', menu = menuList)
